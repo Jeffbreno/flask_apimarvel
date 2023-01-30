1e3953b9b2d44d5e9c333fd97cc5416774e276d2
Jefferson Lopes <jeffbreno@gmail.com>
2023-01-30 18:22:18 -0300

    apimarvel_30012023_1820

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ class Jogo:
         self.category = category
         self.console = console
 
+
 jogo1 = Jogo('Mario', 'Campanha', 'Nitendo')
 jogo2 = Jogo('League Of Legands', 'Estrategia', 'Computador')
 lista = [jogo1, jogo2]
@@ -35,5 +36,19 @@ def criar():
     lista.append(jogo)
 
     return redirect('/')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/autenticar', methods=['POST', ])
+def autenticar():
+    if '123' == request.form['senha']:
+        return redirect('/')
+    else:
+        return redirect('/login')
+
 
 app.run(debug=True, host='localhost', port=5225)
